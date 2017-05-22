f25c805b70322e519a09d76bac1ab9da8227fa6d
Ismail Elouafiq <ismail.elouafiq@telecom-bretagne.eu>
2017-05-22 12:31:10 +0200

    add task structure

--- src/client/pages/NewTask.js
+++ src/client/pages/NewTask.js
@@ -1,17 +1,60 @@
 import React from 'react';
 
+import Time from './Time';
+import CategoryList from './CategoryList';
+
 const styles = {
   wrapper: {
     padding: '30px',
   },
+
+  title: {
+    margin: 'auto',
+    textAlign: 'center',
+    padding: '10px',
+  },
+
+  form: {
+    width: '50%',
+  },
 };
 
-const NewTask = () => {
-  return (
-    <div style={ styles.wrapper }>
-      Create a new Task
-    </div>
-  );
-};
+const categories = [{
+  id: 1,
+  name: 'Babysitting',
+  img: './public/img/ic-kids.svg',
+},
+  {
+    id: 2,
+    name: 'Homework',
+    img: './public/img/ic-study.svg',
+  },
+  {
+    id: 3,
+    name: 'Pets',
+    img: './public/img/ic-dog-walking.svg',
+  },
+];
+
+class NewTask extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: categories,
+    };
+  }
+
+  render() {
+    return (
+          <div style={ styles.wrapper }>
+            <h1 id="task-title" style={ styles.title }>Create a new Task</h1>
+            <div id="task-form" style={ styles.form }>
+              <CategoryList categories={ this.state.categories } />
+              <Time />
+            </div>
+          </div>
+      ); }
+}
 
 export default NewTask;
